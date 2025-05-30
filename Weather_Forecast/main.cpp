a9af07ea330d0283ec1fd9051401d012fe7958e3
yrdf <zzh5653@163.com>
2025-05-30 21:00:39 +0800

    绘制天气预报ui图

--- Weather_Forecast/main.cpp
+++ Weather_Forecast/main.cpp
@@ -1,10 +1,20 @@
 #include "widget.h"
 
 #include <QApplication>
+#include <QFile>
+#include<QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    // 加载样式表
+    QFile styleFile(":/style/stylesheet.qss");
+    if(styleFile.open(QFile::ReadOnly)) {
+        QString style = styleFile.readAll();
+        a.setStyleSheet(style);
+        styleFile.close();
+        qDebug()<<"qss load ok";
+    }
     Widget w;
     w.show();
     return a.exec();
