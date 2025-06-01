7ee58c02601b205c91cde31e515e254c0e804820
yrdf <zzh5653@163.com>
2025-06-01 09:41:11 +0800

    实现窗口拖动和右键关闭

--- Weather_Forecast/widget.cpp
+++ Weather_Forecast/widget.cpp
@@ -1,16 +1,48 @@
 #include "widget.h"
 #include "ui_widget.h"
 
+
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
-    setFixedSize(361,597);
+    setFixedSize(359,598);
     setWindowFlag(Qt::FramelessWindowHint);
+
+    menuQuit = new QMenu(this);
+    menuQuit->setStyleSheet("QMenu::item {color:pink;border-radius:7px}");
+    QAction *closeAct = new QAction(QIcon(":/res/close.png"),tr("退出"),this);
+    menuQuit->addAction(closeAct);
+
+    connect(menuQuit,&QMenu::triggered,this,[=](){
+        this->close();
+    });
 }
 
 Widget::~Widget()
 {
     delete ui;
+}
+
+void Widget::mousePressEvent(QMouseEvent *event)
+{
+    if(event->button() == Qt::RightButton){
+        //qDebug()<<"right mouse button clicked";
+        menuQuit->exec(QCursor::pos());
+    }
+    if(event->button() == Qt::LeftButton){
+        //qDebug()<<"left mouse button clicked";
+        //鼠标当前位置
+        // qDebug()<<event->globalPosition();
+        //窗口当前位置
+        // qDebug()<<this->pos();
+        movepoint = this->pos()-event->globalPosition();
+        //qDebug()<<movepoint;
+    }
+}
+
+void Widget::mouseMoveEvent(QMouseEvent *event)
+{
+    this->move((event->globalPosition()+movepoint).toPoint());
 }
--- Weather_Forecast/widget.h
+++ Weather_Forecast/widget.h
@@ -2,6 +2,10 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QMouseEvent>
+#include<QDebug>
+#include <QMenu>
+#include<QAction>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -16,8 +20,12 @@ class Widget : public QWidget
 public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
-
+protected:
+    void mousePressEvent(QMouseEvent *event)override;
+    void mouseMoveEvent(QMouseEvent *event)override;
 private:
     Ui::Widget *ui;
+    QMenu *menuQuit;
+    QPointF movepoint;
 };
 #endif // WIDGET_H
